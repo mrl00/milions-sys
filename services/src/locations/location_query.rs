97e0e8e4a7ba7847cc7224598c61fdf11bcaeabc
mrl00 <m2l_0@outlook.com>
2025-12-19 22:21:21 -0300

    feat(service): impl locations service

--- services/src/locations/location_query.rs
+++ services/src/locations/location_query.rs
@@ -1,13 +1,38 @@
-use crate::query::Query;
+use sqlx::PgPool;
+use uuid::Uuid;
+
+use crate::locations::models::location::Location;
 
 pub struct LocationQuery;
 
-impl Query for LocationQuery {
-    async fn find_by_id<I, M>(id: I) -> M {
-        todo!()
+impl LocationQuery {
+    pub async fn get_by_id(pool: &PgPool, uuid: Uuid) -> Result<Option<Location>, sqlx::Error> {
+        let r: Option<Location> = sqlx::query_as!(
+            Location,
+            r#"
+            SELECT *
+            FROM locations.tb_location
+            WHERE pk_location = $1
+            "#,
+            &uuid,
+        )
+        .fetch_optional(pool)
+        .await?;
+
+        Ok(r)
     }
 
-    async fn find_all<M>() -> Vec<M> {
-        todo!()
+    pub async fn get_all(pool: &PgPool) -> Result<Vec<Location>, sqlx::Error> {
+        let r: Vec<Location> = sqlx::query_as!(
+            Location,
+            r#"
+            SELECT *
+            FROM locations.tb_location
+            "#,
+        )
+        .fetch_all(pool)
+        .await?;
+
+        Ok(r)
     }
 }
