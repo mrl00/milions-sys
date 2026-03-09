45a7857e6dbbdc88be4f483dde5dedcf596d64a5
mrl00 <m2l_0@outlook.com>
2026-03-08 23:44:23 -0300

    refactor(collab-contact-model): rm unused structs

--- services/src/collaborators/models/collaborator_contact.rs
+++ services/src/collaborators/models/collaborator_contact.rs
@@ -10,17 +10,3 @@ pub struct CollaboratorContact {
     pub ts_collaborator_contact_created_at: NaiveDateTime,
     pub ts_collaborator_contact_updated_at: NaiveDateTime,
 }
-
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct CreateCollaboratorContact {
-    pub fk_collaborator: Uuid,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, sqlx::FromRow)]
-pub struct UpdateCollaboratorContact {
-    pub pk_collaborator_contact: Uuid,
-    pub fk_collaborator: Option<Uuid>,
-    pub fk_contact: Option<Uuid>,
-    pub ts_collaborator_contact_created_at: Option<NaiveDateTime>,
-    pub ts_collaborator_contact_updated_at: Option<NaiveDateTime>,
-}
