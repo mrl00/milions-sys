32e00fc6fbe9f77cf5e344c7f25a72e89e65f676
mrl00 <m2l_0@outlook.com>
2026-03-18 19:10:31 -0300

    refactor: mv clients projects to it own mod

--- repository/src/clients/models/client_project_collaborator.rs
+++
@@ -1,9 +0,0 @@
-#[derive(Debug, sqlx::FromRow)]
-pub struct ClientProjectCollaborator {
-    pub pk_allocated_collaborator: uuid::Uuid,
-    pub idx_allocated_collaborator: i64,
-    pub fk_project: uuid::Uuid,
-    pub fk_collaborator: uuid::Uuid,
-    pub ts_allocated_collaborator_created_at: sqlx::types::chrono::NaiveDateTime,
-    pub ts_allocated_collaborator_updated_at: sqlx::types::chrono::NaiveDateTime,
-}
