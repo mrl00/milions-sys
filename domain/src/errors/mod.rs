b4eaefd08ab86d4a703016e6dc124ef7f72b7ffc
mrl00 <m2l_0@outlook.com>
2026-04-01 17:08:56 -0300

    refactor(domain): remove error types and entities moved to bounded contexts

--- domain/src/errors/mod.rs
+++ domain/src/errors/mod.rs
@@ -1,10 +1,3 @@
-pub mod client_error;
-pub mod contact_error;
 pub mod infra_error;
-pub mod location_error;
-pub mod projects_error;
 
-pub use client_error::ClientError;
-pub use contact_error::ContactError;
 pub use infra_error::InfraError;
-pub use location_error::LocationError;
--- domain/src/models/entities/mod.rs
+++ domain/src/models/entities/mod.rs
@@ -1,2 +1,1 @@
-pub mod client_entity;
 pub mod location_entity;
