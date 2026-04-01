4622e4a744646934366b7aa033f5d081012651d3
mrl00 <m2l_0@outlook.com>
2026-04-01 18:10:41 -0300

    refactor(domain): remove client db models moved to client crate

--- domain/src/models/db/mod.rs
+++ domain/src/models/db/mod.rs
@@ -1,7 +1,3 @@
-pub mod client_address_row;
-pub mod client_contact_row;
-pub mod client_projects_row;
-pub mod client_row;
 pub mod collaborator_contact_row;
 pub mod collaborator_location_row;
 pub mod collaborator_row;
